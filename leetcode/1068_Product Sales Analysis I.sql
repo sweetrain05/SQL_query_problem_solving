44101e607ae6394c361d1a3a8889e54caf3b2df2
Danbi Choi <sweetrain05@hotmail.com>
2024-02-26 19:55:43 -0800

    leetcode sql 50

---
+++ leetcode/1068_Product Sales Analysis I.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/product-sales-analysis-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your MySQL query statement below
+
+SELECT p.product_name, s.year, s.price 
+FROM Sales s
+INNER JOIN Product p
+ON s.product_id = p.product_id
