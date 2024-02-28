27f7d46a11702479ffefef83fce42a88d3d7b711
Danbi Choi <sweetrain05@hotmail.com>
2024-02-27 23:04:44 -0800

    leetcode SQL 50

---
+++ leetcode/620_Not Boring Movies.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT * FROM Cinema
+WHERE id % 2 = 1 AND description != 'boring'
+ORDER BY rating DESC
