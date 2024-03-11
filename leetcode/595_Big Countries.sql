84382ffb2b106bd5ddfe19e75d18985bb0aae11f
Danbi Choi <sweetrain05@hotmail.com>
2024-03-10 23:36:36 -0700

    leetcode sql 50

---
+++ leetcode/595_Big Countries.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+
+select name, population, area
+from World
+where area >=3000000 OR population >= 25000000
