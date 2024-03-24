518fb017a8a37d7565b9f5bab7774607e10bdfdb
Danbi Choi <sweetrain05@hotmail.com>
2024-03-23 21:29:33 -0700

    leetcode sql 50

---
+++ leetcode/1148_Article Views I.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+select author_id as id
+from Views
+where author_id = viewer_id
+group by author_id
+order by id ASC
